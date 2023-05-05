e83b45b0712bee91087190cc73b0ea914f3f850b
shilong233 <127836536+shilong233@users.noreply.github.com>
2023-05-05 22:27:15 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,13 +9,33 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
+    X,y = read_data()
+    
+    # 将 X 展平成二维数组
+    n_samples = X.shape[0]
+    X_flat = X.reshape((n_samples, -1))
+
+    # 设置岭回归的超参数 alpha
+    alpha = -0.1
+
+    # 计算 X_flat 的转置矩阵
+    X_flat_transpose = np.transpose(X_flat)
+
+    # 计算矩阵 X_flat_transpose * X_flat + alpha * I 的逆矩阵
+    inverse = np.linalg.inv(X_flat_transpose.dot(X_flat) + alpha * np.identity(X_flat.shape[1]))
+
+    # 计算最优权重
+    w = inverse.dot(X_flat_transpose).dot(y)
+    return w @ data
+   
+def lasso(data):
     X, y = read_data()
 
-    w = lassotest(X, y, 0.9)
+    w = lassotest(X, y, 0.5)
     return w @ data
 
 
-def lassotest(X, y, alpha, max_iter=5000, tol=1e-19):
+def lassotest(X, y, alpha, max_iter=3000, tol=1e-4):
     # 初始化权重向量
     w = np.zeros(X.shape[1])
 
@@ -40,6 +60,7 @@ def lassotest(X, y, alpha, max_iter=5000, tol=1e-19):
             break
 
     return w
+
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
