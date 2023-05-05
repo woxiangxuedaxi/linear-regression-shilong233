fc0585d1d31e9c1ca882cf63717dc9e076ba3e21
shilong233 <127836536+shilong233@users.noreply.github.com>
2023-05-05 20:18:52 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,10 +9,22 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    pass
+    x,y = read_data()
+    l = -0.2
+    weight = np.dot(np.linalg.inv((np.dot(x.T,x)+np.dot(l,np.eye(6)))),np.dot(x.T,y))
+    return weight @ data
     
 def lasso(data):
-    pass
+    x, Y = read_data()
+    weight = data
+    y = np.dot(weight, x.T)
+    l = 3000
+    rate = 0.00000000001
+    for i in range(int(2e5)):
+        y = np.dot(weight, x.T)
+        dw = np.dot(y - Y, x) + l * np.sign(weight)
+        weight = weight * (1 - (rate * l / 6)) - dw * rate
+    return weight @ data
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
